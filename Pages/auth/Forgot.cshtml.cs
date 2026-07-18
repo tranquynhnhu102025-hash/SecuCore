964809e85349331b1714538bf798bfe487ac048f
Tran_Nhu <tranquynhnhu102025@gmail.com>
2026-07-18 18:41:00 +0700

    code

---
+++ Pages/auth/Forgot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SecuCore.Pages.auth
+{
+    public class ForgotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
